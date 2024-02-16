1650902e52976e13e72c42ad9e6bb3ce4e077a09
Yoan Gvs <yoangovaerts@gmail.com>
2024-02-16 04:36:14 +0100

    IW-92 authUser slice

---
+++ src/features/authUserSlice.js
@@ -0,0 +1,21 @@
+import {createSlice} from '@reduxjs/toolkit';   
+
+export const authUserSlice = createSlice({
+    name: 'authUser',
+    initialState: {
+        token: "",
+    },
+    reducers: {
+        login: (state, action) => {
+            state.token = action.payload.token ;
+            
+        },
+        logout: (state) => {
+            state = {
+                token: "",
+                // user: {}
+            };
+            return state;
+        },
+    }, 
+});
