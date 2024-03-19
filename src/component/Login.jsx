e45de251b96bd86a3ccf72cff2e124f12aacbac8
Yoan Gvs <yoangovaerts@gmail.com>
2024-03-19 03:13:17 +0100

    Fix sign out functionality and update styles

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,15 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, } from 'react-router-dom';
 import { selectCurrentUser, selectCurrentToken, logout } from '../features/authUserSlice';
-
+import { useNavigate } from 'react-router-dom';
 
 
 export const Login = () => {
+  const navigate = useNavigate()
   const token = useSelector(selectCurrentToken);
   const currentUser = useSelector(selectCurrentUser);
   const dispatch = useDispatch();
   const handleSignOut = () => {
     dispatch(logout())
+    navigate("/")
   }
 
 
@@ -23,8 +25,8 @@ export const Login = () => {
               <i className="fa fa-user-circle"></i> {currentUser?.userName}
             </div>
           </Link>
-          <div className="main-nav-item" >
-            <i className="fa fa-sign-out" onClick={handleSignOut} ></i> Sign out
+          <div className="main-nav-item" onClick={handleSignOut}>
+            <i className="fa fa-sign-out"  ></i> Sign out
           </div>
         </div>
       ) : (
